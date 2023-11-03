8206bd680865022ddf714d3198a4b075f7aead50
sorae <46469913+soraeee@users.noreply.github.com>
2023-11-02 22:45:35 -0400

    Rework JSON schema to be less stupid

--- sm2json.py
+++ sm2json.py
@@ -4,6 +4,9 @@ import shutil
 import simfile
 from simfile.ssc import SSCSimfile
 from simfile.types import Chart, Charts, Simfile
+from simfile.notes import NoteData
+from simfile.notes.count import *
+from simfile.timing.displaybpm import displaybpm
 import json
 
 # Things that I probably want to do
@@ -13,17 +16,19 @@ import json
 cwd = os.getcwd()
 
 chartlist = {
-	"charts": [], 
 	"packName": "", 
-	"packBanner": ""}
+	"packBanner": "",
+	"charts": []}
 songIndex = 0
-chartIndex = 0
 
 def getChartData(simfileDir: any):
-	global chartIndex
-	
 	path = simfileDir.simfile_path
+
 	with simfile.mutate(path) as sim:
+		# Get DisplayBPM
+		disp = displaybpm(sim)
+		formattedDisplayBPM = [(str)(disp.min), (str)(disp.max)]
+
 		# Convert banner/jacket names and copy them to the output folder
 		# ash you are saving my goddamn life right now, i was about to do some dumbass pathlib crap to find graphics
 		bnpath = simfileDir.assets().banner
@@ -39,12 +44,39 @@ def getChartData(simfileDir: any):
 			jkext = jkpath.split(".")[-1]
 			shutil.copyfile(jkpath, f"./output/jk-{songIndex}.{jkext}")
 			jkpathFinal = f"./jk-{songIndex}.{bnext}"
+
+		# Create a dict for the metadata to insert into the array that will be converted to JSON later
+		# Definitely the most efficient way to do this, i am highly intelligent :clueless:
+		meta = {
+			"sid": songIndex, # Song ID
+
+			"title": sim.title, # Song title
+			"titletranslit": sim.titletranslit, # Transliterated title
+			"subtitle": sim.subtitle, # Subtitle
+			"subtitletranslit": sim.subtitletranslit, # Transliterated subtitle
+			"artist": sim.artist, # Song artist
+			"artisttranslit": sim.artisttranslit, # Transliterated artist
+			
+			"displaybpm": formattedDisplayBPM, # Song's display BPM
+
+			"bnpath": bnpathFinal, # Path to banner in output folder
+			"jkpath": jkpathFinal, # Path to jacket in output folder
+			# We skip backgrounds because that's more filesize and also I can't think of a use case where you would want bg over bn x_x
+			# (maybe a bad idea)
+
+			"difficulties": []
+		}
 	
 		# Checking for mods and gimmicks
 		hasmods = True if (sim.fgchanges != None) | (sim.attacks != None) else False
 
+		# Parse individual difficulties
 		for chart_ in sim.charts:
 			chart: Chart = chart_ # typing workaround
+
+			# Count number of notes
+			notedata = NoteData(chart)
+			notecount = count_steps(notedata)
 			
 			# Checking for ssc specific fields
 			credit = chart.credit if chart.credit != None else ""
@@ -53,29 +85,15 @@ def getChartData(simfileDir: any):
 			# Checking for difficulty-specific mods
 			if chart.attacks != None: hasmods = True
 
-			# Create a dict for the metadata to insert into the array that will be converted to JSON later
-			# Definitely the most efficient way to do this, i am highly intelligent :clueless:
-			meta = {
-				"sid": songIndex, # Song ID
-				"cid": chartIndex, # Chart ID, can differ from Song ID when a chart has multiple difficulties
+			# Difficulty specific metadata
+			diff = {
 
 				"slot": chart.difficulty, # Difficulty slot of the chart - Challenge, Hard, Medium, etc 
 				"type": chart.stepstype, # Steps type - Singles, Doubles, etc
 				"difficulty": chart.meter, # The difficulty number assigned to the chart. Always 100% accurate and never subject to community debate 
+				
+				"notecount": notecount, # Number of notes, as specified by StepMania ingame
 
-				"title": sim.title, # Song title
-				"titletranslit": sim.titletranslit, # Transliterated title
-				"subtitle": sim.subtitle, # Subtitle
-				"subtitletranslit": sim.subtitletranslit, # Transliterated subtitle
-				"artist": sim.artist, # Song artist
-				"artisttranslit": sim.artisttranslit, # Transliterated artist
-
-				"bnpath": bnpathFinal, # Path to banner in output folder
-				"jkpath": jkpathFinal, # Path to jacket in output folder
-				# We skip backgrounds because that's more filesize and also I can't think of a use case where you would want bg over bn x_x
-				# (maybe a bad idea)
-
-				"displaybpm": sim.displaybpm, # Song's display BPM
 				"description": chart.description, # Chart's description field
 				"credit": credit, # Chart specific credit - ssc only
 				"chartname": chartname, # Chart's chartname - ssc only
@@ -90,8 +108,10 @@ def getChartData(simfileDir: any):
 				# I considered a "hasgimmicks" field, but I feel like that's considerably less useful due to stuff like BermudaTriangle or CMOD ok charts
 			}
 
-			chartlist["charts"].append(meta)
-			chartIndex += 1
+			meta["difficulties"].append(diff)
+		
+		# Add final song metadata w/ all difficulties to overall chart list 
+		chartlist["charts"].append(meta)
 
 def main():
 	global songIndex
@@ -103,12 +123,8 @@ def main():
 	if not os.path.exists(f"{cwd}/output"):
 		os.makedirs(f"{cwd}/output")
 
-	# Parse simfiles for metadata - prioritize ssc
-	simPack = simfile.dir.SimfilePack(packPath)
 	try:
-		for simfileDir in simPack.simfile_dirs():
-			getChartData(simfileDir)
-			songIndex += 1
+		simPack = simfile.dir.SimfilePack(packPath)
 		
 		# Get pack banner and copy it to output folder
 		packbnPath = simPack.banner()
@@ -120,6 +136,11 @@ def main():
 		# Append pack metadata to JSON
 		chartlist["packName"] = simPack.name
 		chartlist["packBanner"] = packbnPathFinal
+		
+		# Parse simfiles for metadata - prioritize ssc
+		for simfileDir in simPack.simfile_dirs():
+			getChartData(simfileDir)
+			songIndex += 1
 
 		# Convert the resulting chart list to a JSON string
 		chartJSON = json.dumps(chartlist)
